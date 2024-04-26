cd51216e1ad71f2b85b7b76727071d2ee224aa0e
Alex <alex.kleinman@gmail.com>
2024-04-26 09:39:49 -0400

    added javascript variable for html canvas

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
