1f3aa451e27e0041335935ea94dcf72633d4b491
Alex <alex.kleinman@gmail.com>
2024-04-26 10:32:42 -0400

    initial bouncing code

--- script.js
+++ script.js
@@ -3,22 +3,38 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
 
+//Game Code Start
+
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const ballRadius = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#81B4FF";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+  }
+  
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < 0) {
+        dx = -dx;
+      }
+      
+      if (y + dy > canvas.height - ballRadius || y + dy < 0) {
+        dy = -dy;
+      }
     x += dx;
     y += dy;
   }
+
   setInterval(draw, 10);
 
