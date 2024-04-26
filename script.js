97011d11e942b3702b13b67f7d9134c7ca133dc6
Alex <alex.kleinman@gmail.com>
2024-04-26 09:41:22 -0400

    added player avatar

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.querySelector("body").appendChild(h2);
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
