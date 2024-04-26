a35a5ef1d640859fda5bb79a7fe44f337d139072
Alex <alex.kleinman@gmail.com>
2024-04-26 10:11:52 -0400

    initial motion code for avatar

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ document.querySelector("body").appendChild(h2);
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#81B4FF";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+  }
+  setInterval(draw, 10);
+
