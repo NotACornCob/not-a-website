a781b99d68021d39177ac79c551fdbcea3c16aac
Alex <alex.kleinman@gmail.com>
2024-05-04 15:00:13 -0400

    bigger ball

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ document.querySelector("body").appendChild(h2);
 
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-const ballRadius = 10;
+const ballRadius = 30;
 const colors = ["red","green","yellow","blue","orange"];
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
+let dx = 3;
+let dy = -3;
 let i =0;
 
 function randomColor() {
@@ -24,7 +24,7 @@ function randomColor() {
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 4);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = colors[i];
     ctx.fill();
     ctx.closePath();
