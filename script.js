01d3c7f6382f826290e87ba842c95d35d54b9c7e
Alex <alex.kleinman@gmail.com>
2024-05-04 14:35:38 -0400

    random colors activated

--- script.js
+++ script.js
@@ -8,15 +8,24 @@ document.querySelector("body").appendChild(h2);
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const ballRadius = 10;
+const colors = ["red","green","yellow","blue","orange"];
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+let i =0;
+
+function randomColor() {
+  if (i >= colors.length - 1) {
+    return i = 0;
+  }
+  return i++;
+}
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 4);
+    ctx.fillStyle = colors[i];
     ctx.fill();
     ctx.closePath();
   }
@@ -25,16 +34,16 @@ function drawBall() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
 
-    if (x + dx > canvas.width - ballRadius || x + dx < 0) {
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
-      }
-      
-      if (y + dy > canvas.height - ballRadius || y + dy < 0) {
+        return randomColor(i);
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
-      }
+        return randomColor(i);
+    }
     x += dx;
     y += dy;
   }
 
   setInterval(draw, 10);
-
